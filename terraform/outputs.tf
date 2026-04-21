b8fa36bd9db694c70fadad243ccfff612396206f
Ludw1gd <tioirin2004@gmail.com>
2026-04-21 18:08:30 +0700

    chore: basic app initial

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "jenkins_vm_public_ip" {
+    description = "Public IP Jenkins Server"
+    value       = "..." # akan diisi logic main.tf
+}
+
+output "worker_vm_public_ip" {
+    description = "Public IP Target Worker Server"
+    value       = "..." # Akan diisi logic main.tf
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+    default = "rg-devops-miniproject"
+}
+variable "location" {
+    default = "East US"
+}
+variable "admin_username" {
+    default = "azureuser"
+}
+# menggunakan SSH Key, bukan password
